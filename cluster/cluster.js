7bb7a7b6c0f5ec9b527d8b9befafd5bab8705195
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-02-06 01:12:44 +0800

    first step..

--- cluster/cluster.js
+++ cluster/cluster.js
@@ -4,8 +4,6 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
-var user = require('./routes/user');
 var http = require('http');
 var path = require('path');
 
@@ -13,8 +11,6 @@ var app = express();
 
 // all environments
 app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
@@ -28,8 +24,13 @@ if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
-app.get('/users', user.list);
+app.get('/', function(req, res) {
+    res.send(400, 'Bad request');
+});
+
+app.get('/:hash', function(req, res) {
+
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
--- master/master.js
+++ master/master.js
@@ -4,10 +4,11 @@
  */
 
 var express = require('express');
-var routes = require('./routes');
 var http = require('http');
 var path = require('path');
 var config = require('./config.js');
+var crypto = require('crypto');
+var fs = require('fs');
 
 var app = express();
 
@@ -18,23 +19,59 @@ app.set('view engine', 'ejs');
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded());
+app.use(express.bodyParser({
+    keepExtensions: false,
+    uploadDir: "tmpdata"
+}));
 app.use(express.methodOverride());
-app.use(app.router);
+// app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
-
-// handle 404
-app.use(function(req, res) {
-    res.status(404);
-    res.render();
-});
 
 // development only
 if ('development' == app.get('env')) {
   app.use(express.errorHandler());
 }
 
-app.get('/', routes.index);
+app.get('/', function(req, res) {
+    res.render('index', {
+        siteName: config.siteName
+    });
+});
+
+app.post('/', function(req, res) {
+    // console.log(req.files.uploadfile);
+
+    // hash file with sha256
+    var sha256sum = crypto.createHash('sha256');
+    var filehash = fs.ReadStream(req.files.uploadfile.path);
+    filehash.on('data', function(d) {
+        sha256sum.update(d);
+    });
+    filehash.on('end', function() {
+        var d = sha256sum.digest('hex');
+        console.log(d + ' ' + req.files.uploadfile.name);
+        // move file to storage location
+        fs.rename(req.files.uploadfile.path, 'files/' + d, function() {
+            // notify clusters to fetch file
+
+            // return client with download path
+            res.send(200, config.clusterurl + d);
+        });
+    });
+});
+
+app.post('/syncfile', function(req, res) {
+    if (req.body.accesskey != config.clusterkey) {
+        return res.send(401);
+    } else {
+        res.sendfile('files/' + req.body.filehash);
+    }
+});
+
+// handle 404
+app.use(function(req, res) {
+    res.send(404, 'My files gone?! :-O');
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
