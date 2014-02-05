e471627699fa2dbb2f8282c8db0f6f6ba0ae72c3
Phoenix Nemo <mrphoenixlzx@gmail.com>
2014-02-05 22:15:31 +0800

    initial commit

---
+++ master/config.js
@@ -0,0 +1,16 @@
+module.exports={
+
+    // set clusters information below, notice all clusters will be in http protocol.
+    // cluster public access url
+    clusterurl: 'http://files.example.com',
+    // clusters sync url
+    clusters: ['http://c1.files.example.com', 'c2.files.example.com', 'c3.files.example.com', 'c4.files.example.com'],
+    // sync key
+    clusterkey: 'Up3os8aeaeSuch1nJaecie2SeeWahce9poi7Vu9o',
+
+    // site settings
+    siteName: 'directshare',
+
+    // server settings
+    port: 3000
+};
